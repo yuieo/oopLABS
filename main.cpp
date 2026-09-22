016455b806b05a266eaa8b50eb629714981eba06
yuiro <yueeiyueei>
2026-09-23 01:57:16 +0500

    Добавлено заполнение массива с клавиатуры через ссылку

--- main.cpp
+++ main.cpp
@@ -6,12 +6,28 @@
 #include <iostream>
 
 /**
+ * @brief Заполняет массив числами с клавиатуры.
+ * @param arr Ссылка на массив из 10 целых чисел.
+ */
+
+void fillArray(int (&arr)[10])
+{
+    std::cout << "Введите 10 целых чисел:\n";
+
+    for (int i = 0; i < 10; ++i) {
+        std::cin >> arr[i];
+    }
+}
+
+/**
  * @brief Точка входа в программу.
  * @return Код завершения: 0 — успешное выполнение.
  */
 int main()
 {
     int numbers[10]{};
+
+    fillArray(numbers);
 
     std::cout << "Первый элемент: " << numbers[0] << '\n';
 
